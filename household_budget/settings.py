89599b8ff394995463221ac60dea6043624bcc7c
nadev240304 <ths4w4u8@gmail.com>
2024-04-07 20:52:00 +0900

    settings訂正

--- household_budget/settings.py
+++ household_budget/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 import environ
-from decouple import config
+# from decouple import config
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
