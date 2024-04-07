d1940dc2b3c76242bafe5cc5c748bba293387c26
nadev240304 <ths4w4u8@gmail.com>
2024-04-07 20:29:14 +0900

    superuser　訂正

--- accounts/management/commands/superuser.py
+++ accounts/management/commands/superuser.py
@@ -2,12 +2,12 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.conf import settings
 
-User = get_user_model()
+CustomUser = get_user_model()
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username=settings.SUPERUSER_NAME).exists():
-            User.objects.create_superuser(
+        if not CustomUser.objects.filter(username=settings.SUPERUSER_NAME).exists():
+            CustomUser.objects.create_superuser(
                 username=settings.SUPERUSER_NAME,
                 email=settings.SUPERUSER_EMAIL,
                 password=settings.SUPERUSER_PASSWORD
