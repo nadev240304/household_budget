67a703c3e428e55596db6137382aeaf37fc1fc5b
nadev240304 <ths4w4u8@gmail.com>
2024-04-08 11:47:21 +0900

    settings訂正

--- household_budget/settings.py
+++ household_budget/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-of%ad_!)=w!5!)y3(1&j4yn8^a3uub!&c0%4o=37h@&+f7%&y='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["nagasaki-portfolio.onrender.com"]
 
 
 # Application definition
