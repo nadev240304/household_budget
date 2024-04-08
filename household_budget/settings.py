cf3f85ff80483034fcc0511fe090025f18dbc98c
nadev240304 <ths4w4u8@gmail.com>
2024-04-08 12:15:17 +0900

    settings訂正

--- household_budget/settings.py
+++ household_budget/settings.py
@@ -112,7 +112,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
